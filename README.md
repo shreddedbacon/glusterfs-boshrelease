6bca5ebd1ecb55ee4d03cf8cc1fd4b03ba887414
Ben Jackson <b@benjackson.email>
2019-01-25 21:42:24 +1100

    made to work with source code libraries and xenial-97.x stemcells

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # GlusterFS BOSH Release
 
+Compiles GlusterFS as below on Ubuntu Xenial Stemcells 97.x
+```
+GlusterFS configure summary
+===========================
+FUSE client          : yes
+Infiniband verbs     : no
+epoll IO multiplex   : yes
+fusermount           : yes
+readline             : yes
+georeplication       : yes
+Linux-AIO            : yes
+Enable Debug         : no
+Enable ASAN          : no
+Block Device xlator  : no
+glupy                : no
+Use syslog           : yes
+XML output           : yes
+Encryption xlator    : yes
+Unit Tests           : no
+Track priv ports     : yes
+POSIX ACLs           : yes
+Data Classification  : yes
+SELinux features     : yes
+firewalld-config     : no
+Events               : yes
+EC dynamic support   : x64 sse avx
+Use memory pools     : yes
+Nanosecond m/atimes  : yes
+Server components    : yes
+Legacy gNFS server   : no
+IPV6 default         : no
+Use TIRPC            : missing
+With Python          : 3.5
+Cloudsync            : yes
+```
+
 
--- get_blobs.sh
+++ get_blobs.sh
@@ -19,10 +19,13 @@ https://www.openfabrics.org/downloads/verbs/libibverbs-1.2.1.tar.gz
 https://www.openfabrics.org/downloads/rdmacm/librdmacm-1.1.0.tar.gz
 https://lttng.org/files/urcu/userspace-rcu-0.10.2.tar.bz2
 https://www.cpan.org/modules/by-module/XML/XML-Parser-2.44.tar.gz
-https://src.fedoraproject.org/lookaside/extras/lvm2/LVM2.2.02.183.tgz/sha512/3947523c6b0862ada40677ed171ed0cf95e070119e377296fc5ccb153e9c4212d34c0b16a887dbd68ccf265525345dabfed2aa59fb3604555429a7e2ecfff4d7/LVM2.2.02.183.tgz
+https://src.fedoraproject.org/lookaside/extras/lvm2/LVM2.2.02.130.tgz/e7ae07e413120713549cc6dd2a558c75/LVM2.2.02.130.tgz
 http://download.savannah.gnu.org/releases/acl/acl-2.2.52.src.tar.gz
 http://download.savannah.gnu.org/releases/attr/attr-2.4.47.src.tar.gz
 https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.1.tar.gz
+https://ftp.gnu.org/pub/gnu/gettext/gettext-0.19.8.tar.gz
+https://sqlite.org/2018/sqlite-autoconf-3220000.tar.gz
+http://xmlsoft.org/sources/libxml2-2.9.8.tar.gz
 "
 BUILD_TOOL_URLS="https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
 https://ftp.gnu.org/gnu/automake/automake-1.16.1.tar.gz
@@ -37,6 +40,13 @@ do
     bosh add-blob .downloads/${i##*/} gluster-build-deps/${i##*/}
   fi
 done
+
+UTIL_LINUX_URL="https://github.com/karelzak/util-linux/archive/v2.33.1.tar.gz"
+if [ ! -e ".downloads/util-linux-2.33.1.tar.gz" ]
+then
+  curl -J -L -o .downloads/util-linux-2.33.1.tar.gz $UTIL_LINUX_URL
+  bosh add-blob .downloads/util-linux-2.33.1.tar.gz gluster-build-deps/util-linux-2.33.1.tar.gz
+fi
 
 for i in $BUILD_TOOL_URLS
 do
@@ -57,6 +67,6 @@ fi
 PYTHON="https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz"
 if [ ! -e ".downloads/Python-3.5.2.tgz" ]
 then
-  curl -J -L -o .downloads/Python-3.5.2.tgz $GLUSTER_FS
+  curl -J -L -o .downloads/Python-3.5.2.tgz $PYTHON
   bosh add-blob .downloads/Python-3.5.2.tgz python/Python-3.5.2.tgz
 fi
